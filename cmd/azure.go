71dfd4eb3b3aa6a0f2c8f261c71cdff3b433c72d
Dongmin (Reo) Lee <reoim10@gmail.com>
2019-09-07 02:40:42 +0900

    Azure Done!!

--- cmd/azure.go
+++ cmd/azure.go
@@ -28,33 +28,73 @@ import (
 // azureCmd represents the azure command
 var azureCmd = &cobra.Command{
 	Use:   "azure",
-	Short: "Ping Azure regions",
-	Long:  `Ping Azure regions and print latency`,
+	Short: "Check latencies of Azure regions.",
 	Run: func(cmd *cobra.Command, args []string) {
-		// Init tabwriter
-		tr := tabwriter.NewWriter(os.Stdout, 40, 8, 2, '\t', 0)
-		fmt.Fprintf(tr, "Region Code\tRegion Name\tLatency")
-		fmt.Fprintln(tr)
-		fmt.Fprintf(tr, "------------------------------\t------------------------------\t------------------------------")
-		fmt.Fprintln(tr)
 
-		// Flush tabwriter
-		tr.Flush()
+		fmt.Println("")
+		if list {
 
-		for r, i := range azure.AZUREEndpoints {
-			p := ping.PingDto{
-				Region:  r,
-				Name:    azure.AZURERegions[r],
-				Address: i,
+			// Init tabwriter
+			tr := tabwriter.NewWriter(os.Stdout, 40, 8, 2, '\t', 0)
+			fmt.Fprintf(tr, "Azure Region Code\tAzure Region Name")
+			fmt.Fprintln(tr)
+			fmt.Fprintf(tr, "------------------------------\t------------------------------")
+			fmt.Fprintln(tr)
+			for r, n := range azure.AZURERegions {
+				fmt.Fprintf(tr, "[%v]\t[%v]", r, n)
+				fmt.Fprintln(tr)
 			}
-			p.Ping()
+			// Flush tabwriter
+			tr.Flush()
+		} else if len(args) == 0 {
+
+			// Init tabwriter
+			tr := tabwriter.NewWriter(os.Stdout, 40, 8, 2, '\t', 0)
+			fmt.Fprintf(tr, "Azure Region Code\tAzure Region Name\tLatency")
+			fmt.Fprintln(tr)
+			fmt.Fprintf(tr, "------------------------------\t------------------------------\t------------------------------")
+			fmt.Fprintln(tr)
+
+			// Flush tabwriter
+			tr.Flush()
+
+			for r, i := range azure.AZUREEndpoints {
+				p := ping.PingDto{
+					Region:  r,
+					Name:    azure.AZURERegions[r],
+					Address: i,
+				}
+				p.Ping()
+			}
+			fmt.Println("")
+			fmt.Println("You can also add region after command if you want http trace information of the specific region")
+			fmt.Println("ex> pingcloud-cli azure centralus")
+		} else {
+			for _, r := range args {
+				if i, ok := azure.AZUREEndpoints[r]; ok {
+					p := ping.PingDto{
+						Region:  r,
+						Name:    azure.AZURERegions[r],
+						Address: i,
+					}
+					p.VerbosePing()
+				} else {
+					fmt.Printf("Region code [%v] is wrong.  To check available region codes run the command with -l or --list flag\n", r)
+					fmt.Println("Usage: pingcloud-cli azure -l")
+					fmt.Println("Usage: pingcloud-cli azure --list")
+
+				}
+			}
+
 		}
+		fmt.Println("")
+
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(azureCmd)
-
+	azureCmd.Flags().BoolVarP(&list, "list", "l", false, "List all available regions")
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
